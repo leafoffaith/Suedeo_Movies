2b141407b8045e0fd8a9b4b49494db075bf91d7f
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-11-13 02:35:53 +0000

    Added and connected mongoDB
    Have an error popping up for https://github.com/facebook/create-react-app/issues/12035 this issue

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
-const dotenv = require('dotenv').config()
+const colors = require('colors')
+const dotenv = require('dotenv').config({ path: '.env' })
 const { errorHandler } = require('./middleware/errorMiddleware')
-const PORT = process.env.PORT || 3001
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 3000
+
+connectDB()
 
 const app = express()
 
