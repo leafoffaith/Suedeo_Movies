ce7de6930309fd9650096fc44939554ac62124b7
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-11-13 01:15:23 +0000

    Added async handler package

--- backend/controllers/testController.js
+++ backend/controllers/testController.js
@@ -1,35 +1,37 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc    Get tests
 // @route   GET /api/tests
 // @access  Private
-const getTests = (req, res) => {
+const getTests = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Get tests' })
-}
+})
 
 // @desc    Set test
 // @route   SET /api/tests
 // @access  Private
-const setTest = (req, res) => {
+const setTest = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     // Test error
     res.status(400)
     throw new Error('Please add a text field')
   }
   res.status(200).json({ message: 'Set test' })
-}
+})
 
 // @desc    Update test
 // @route   PUT /api/tests/:id
 // @access  Private
-const updateTest = (req, res) => {
+const updateTest = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update test ${req.params.id}` })
-}
+})
 
 // @desc    Delete test
 // @route   DELETE /api/tests/:id
 // @access  Private
-const deleteTest = (req, res) => {
+const deleteTest = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete test ${req.params.id}` })
-}
+})
 
 module.exports = {
   getTests,
