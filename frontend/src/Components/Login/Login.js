db5a9738b4040c48cbbac08b8935aab7711e618a
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-11-13 21:42:58 +0000

    Merged branches

--- frontend/src/Components/Login/Login.js
+++ frontend/src/Components/Login/Login.js
@@ -7,21 +7,41 @@ export default function Login() {
   const { password, passwordOnChange } = useInput('')
 
   return (
-    <div className='login__container'>
-      Login Component
-      <div className='login__email_container'>
-        <label>Email Address</label>
-        <input type={'text'} value={email} onChange={(e) => emailOnchange(e)} />
+    <div className='login_page__container'>
+      {/* This is the left hand side of login page */}
+      <div className='left'>
+        <div className='left__content'>
+          <h1>Get The Best Movie Recommendations</h1>
+          <h3 className='left_desc'>
+            ...in your fingertips. We will provide you with the next best movie
+            you can watch by knowing about your movie preferences. Quick, give
+            it a try now
+          </h3>
+          <button className='btn btn-xl cta-btn signup_btn'>Sign up!</button>
+        </div>
       </div>
-      <div className='login__email_container'>
-        <label>Password</label>
-        <input
-          type={'password'}
-          value={password}
-          onChange={(e) => passwordOnChange(e)}
-        />
+      <div className='login__container'>
+        <h2 className='heading'>Login</h2>
+        <div className='login__email_container text-input-field-container'>
+          <label>Email Address</label>
+          <input
+            type={'text'}
+            value={email}
+            onChange={(e) => emailOnchange(e)}
+            className='text-input'
+          />
+        </div>
+        <div className='login__email_container text-input-field-container'>
+          <label>Password</label>
+          <input
+            type={'password'}
+            value={password}
+            onChange={(e) => passwordOnChange(e)}
+            className='text-input'
+          />
+        </div>
+        <button className='btn btn-lg'>Login</button>
       </div>
-      <button>Login</button>
     </div>
   )
 }
