3515299b0037a8462179b789e00e62d6c42cd0fc
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-11-28 17:16:04 +0000

    Restored files

---
+++ frontend/src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="container">
+        <span>https://github.com/reeceawalsh/Suedeo_Movies</span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
