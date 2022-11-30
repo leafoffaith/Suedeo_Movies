3218691f54d0aedb11f50e534c7ceacef12b59a6
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-11-30 12:31:54 +0000

    Wrote a method which displays a x/10 user rating as x/5 stars

--- frontend/src/Components/List/List.js
+++ frontend/src/Components/List/List.js
@@ -1,45 +1,27 @@
 import MovieItem from "../MovieItem/MovieItem";
+import axios from "axios"
 import "./list.scss";
 import { useEffect, useState} from "react"
 
-const Movies = [
-  {
-    title: 'Mr Man'
-  }
-]
 export default function List({streamingProvider}) {
   const [movies, setMovies] = useState([])
 
   useEffect(() => {
     // fetch movie
-    const movieData = Movies
-    setMovies(movieData)
+    axios.get('https://api.themoviedb.org/3/discover/movie?api_key=dbe4608d19182e24de51d5d4e342e8df&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate').then((response) => {
+      setMovies(response.data.results)
+    }).catch((err)=>{
+      console.log(err)
+    })
   },[])
 
   return (
     <div className="list">
       <div className="wrapper">
-        <div className="container">
-        <div id="movie-item-container">
-          <MovieItem index={0} />
-          <MovieItem index={1} />
-          <MovieItem index={2} />
-          <MovieItem index={3} />
-          <MovieItem index={4} />
-          <MovieItem index={5} />
-          <MovieItem index={6} />
-          <MovieItem index={7} />
-          <MovieItem index={8} />
-          <MovieItem index={9} />
-          <MovieItem index={10} />
-          <MovieItem index={11} />
-          <MovieItem index={12} />
-          <MovieItem index={13} />
-          <MovieItem index={14} />
-          </div>
-          {/* <ArrowForwardIosIcon
-          id="nextMovies"
-          size="medium" /> */}
+       <div className="container">
+        {movies.map((movie, index) => {
+          return <MovieItem key={index} {...movie}/>
+        })}
         </div>
       </div>
     </div>
--- frontend/src/Components/MovieItem/MovieItem.js
+++ frontend/src/Components/MovieItem/MovieItem.js
@@ -7,7 +7,42 @@ import StarHalfIcon from '@mui/icons-material/StarHalf';
 import StarOutlineIcon from '@mui/icons-material/StarOutline';
 import Star from "@mui/icons-material/Star";
 
-export default function MovieItem({ index, imageURL, title, duration, parentalRating, date, desc, genre, userRating }) {
+const getPosterURL = (poster_path) => {
+  return `https://www.themoviedb.org/t/p/w440_and_h660_face/${poster_path}`
+}
+
+const description = (overview) => {
+  if (overview.length > 150) {
+    return overview.slice(0, 150) + "..."
+  } else {
+    return overview
+  }
+}
+
+const starRating = (vote_average) => {
+  const stars = [
+    <StarOutlineIcon className="star" />,
+     <StarOutlineIcon className="star" />,
+     <StarOutlineIcon className="star" />,
+ <StarOutlineIcon className="star" />,
+<StarOutlineIcon className="star" />,
+  ]
+  const outOfFive = vote_average / 2
+  const decimalPart = outOfFive.toString().split('.')[1]
+  for (let i = 0; i + 0.9 < outOfFive; i++) {
+    stars[i] = <StarIcon className="star" />
+  }
+  if (decimalPart != 0 && decimalPart >= 5) {
+    stars[outOfFive] = <StarHalfIcon className="star" />
+  }
+  for (let i = outOfFive; i < 5; i++) {
+    stars[i] = <StarOutlineIcon className="star" />
+  }
+  return stars
+
+}
+
+export default function MovieItem({ index, poster_path, title, duration, parentalRating, release_date, overview, genre, vote_average }) {
   const [isHovered, setIsHovered] = useState(false);
   return (
     <div
@@ -17,7 +52,7 @@ export default function MovieItem({ index, imageURL, title, duration, parentalRa
       onMouseLeave={() => setIsHovered(false)}
     >
       <img
-        src={imageURL}
+        src={getPosterURL(poster_path)}
         alt=""
       />
       {isHovered && (
@@ -27,28 +62,23 @@ export default function MovieItem({ index, imageURL, title, duration, parentalRa
             <div id="itemInfoTop">
               <span id="duration">{duration}</span>
               <span id="rating">{parentalRating}</span>
-              <span id="year">{date}</span>
+              <span id="year">{release_date}</span>
             </div>
             <div id="desc">
-              {desc}
+            <p>{description(overview)}</p>
             </div>
             <div id="genre">{genre}</div>
             <div id="rating">
-            <ThumbDownIcon class="rating" size="small"/>
-            <ThumbUpIcon class="rating" size="small"/>
+            <ThumbDownIcon className="rating" size="small"/>
+            <ThumbUpIcon className="rating" size="small"/>
           </div>
           </div>
         </>
       )}
       <div id="bottom">
-        <span id="firstTitle">Happy Feet</span>
+        <span id="firstTitle">{title}</span>
         <div id="stars">
-          {userRating}
-          <StarIcon className="star" />
-          <StarIcon className="star" />
-          <StarIcon className="star" />
-          <StarHalfIcon className="star" />
-          <StarOutlineIcon className="star" />
+          {starRating(vote_average)}
         </div>
       </div>
     </div>
