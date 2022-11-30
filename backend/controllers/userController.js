e26b553d53edc3e87a80319846cc4e9f7b0b25d4
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-11-30 16:18:08 +0000

    Complete CRUD functionality with authorisation and authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,7 +22,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   // Hash password
-  const salt = await bcrypt.genSalt(10)
+  const salt = await bcrypt.genSalt(10) // To hash the password
   const hashedPassword = await bcrypt.hash(password, salt)
 
   // Create user
