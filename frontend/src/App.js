92a61273a9b29fd5125962a5385a84c2a79ec26f
ai-is-awesome <irock0093@gmail.com>
2022-11-30 03:36:56 +0000

    setup react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,13 @@ import "./styles/App.css";
 import "./styles/scss/main.scss";
 import Login from "./Components/LoginForm/Login";
 import LoginPage from "./Pages/LoginPage";
+import Home from "./Pages/Home/Home";
 
 function App() {
   const [data, setData] = useState(null);
   const [user, setuser] = useState(null);
 
-  return (
-    <div>
-      <LoginPage />
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
