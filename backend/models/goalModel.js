1f9334087f5e2c43c90032cb0cdf3d3c5f5ef2db
ai-is-awesome <irock0093@gmail.com>
2022-12-01 15:01:08 +0000

    working on integrating user login frontend

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,23 +1,20 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const goalSchema = mongoose.Schema(
   {
-<<<<<<< HEAD
-=======
     user: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
-      ref: 'User'
+      ref: "User",
     },
->>>>>>> 62e134cd48a85df752760c4faa803ca0c65a36e4
     text: {
       type: String,
-      required: [true, 'Please add a text value'],
+      required: [true, "Please add a text value"],
     },
   },
   {
     timestamps: true,
   }
-)
+);
 
-module.exports = mongoose.model('Goal', goalSchema)
+module.exports = mongoose.model("Goal", goalSchema);
--- backend/routes/goalRoute.js
+++ backend/routes/goalRoute.js
@@ -1,23 +1,16 @@
-const express = require('express')
-const { update } = require('tar')
-const router = express.Router()
+const express = require("express");
+const { update } = require("tar");
+const router = express.Router();
 const {
   getGoals,
   setGoal,
   updateGoal,
   deleteGoal,
-} = require('../controllers/goalController')
+} = require("../controllers/goalController");
 
-<<<<<<< HEAD
-router.route('/').get(getGoals).post(setGoal)
-router.route('/:id').put(updateGoal).delete(deleteGoal)
+const { protect } = require("../middleware/authMiddleware");
 
-module.exports = router
-=======
-const { protect } = require('../middleware/authMiddleware')
+router.route("/").get(protect, getGoals).post(protect, setGoal);
+router.route("/:id").put(protect, updateGoal).delete(protect, deleteGoal);
 
-router.route('/').get(protect, getGoals).post(protect, setGoal)
-router.route('/:id').put(protect, updateGoal).delete(protect, deleteGoal)
-
-module.exports = router
->>>>>>> 62e134cd48a85df752760c4faa803ca0c65a36e4
+module.exports = router;
