c5e72bdd49ff148d5ca5dcab64d7d46a116462f4
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-12-01 13:01:33 +0000

    Added more streaming providers

--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -3,11 +3,16 @@ import "./home.scss";
 import List from "../../Components/List/List";
 import Footer from "../../Components/Footer/Footer"
 
+const movieLists = ['Netflix', 'Amazon Prime', 'Disney+', 'Hulu', 'YouTube', 'HBO', 'Apple TV', 'Peacock', 'ITV']
+
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
-      <h1 className="streamingProvider">Netflix</h1>
+      {movieLists.map((item, index) => {
+        return <><h1 className="streamingProvider">{item}</h1><List pages={index + 1} /></>
+      })}
+      {/* <h1 className="streamingProvider">Netflix</h1>
       <List pages="2"/>
       <h1 className="streamingProvider">Amazon Prime</h1>
       <List pages="3"/>
@@ -15,6 +20,14 @@ const Home = () => {
       <List pages="4"/>
       <h1 className="streamingProvider">Hulu</h1>
       <List pages="5"/>
+      <h1 className="streamingProvider">Netflix</h1>
+      <List pages="6"/>
+      <h1 className="streamingProvider">Amazon Prime</h1>
+      <List pages="7"/>
+      <h1 className="streamingProvider">Disney+</h1>
+      <List pages="8"/>
+      <h1 className="streamingProvider">Hulu</h1>
+      <List pages="9"/> */}
       <Footer />
     </div>
   );
