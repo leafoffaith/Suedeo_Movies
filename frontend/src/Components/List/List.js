eba89b63d7a56a46d4d6c44430a44e33341587d4
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-12-08 01:32:46 +0000

    Added some more pages of movies for each list, just copied and pasted code for now, to see how it looks. Might need an extra row per streaming provider

--- frontend/src/Components/List/List.js
+++ frontend/src/Components/List/List.js
@@ -35,6 +35,9 @@ const convertProvider = (provider) => {
 }
 export default function List(pages) {
   const [movies, setMovies] = useState([])
+  const [movies2, setMovies2] = useState([])
+  const [movies3, setMovies3] = useState([])
+  const [movies4, setMovies4] = useState([])
   let pageNumber = pages.pages + 1
   let provider = convertProvider(pages.provider)
   
@@ -45,13 +48,37 @@ export default function List(pages) {
       }).catch((err) => {
         console.log(err)
       })
-  },
+      axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=dbe4608d19182e24de51d5d4e342e8df&language=en-US&region=GB&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pageNumber += 1}&with_watch_providers=${provider}&watch_region=GB&with_watch_monetization_types=flatrate`).then((response) => {
+        setMovies2(response.data.results)
+      }).catch((err) => {
+        console.log(err)
+      })
+      axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=dbe4608d19182e24de51d5d4e342e8df&language=en-US&region=GB&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pageNumber += 1}&with_watch_providers=${provider}&watch_region=GB&with_watch_monetization_types=flatrate`).then((response) => {
+        setMovies3(response.data.results)
+      }).catch((err) => {
+        console.log(err)
+      })
+      axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=dbe4608d19182e24de51d5d4e342e8df&language=en-US&region=GB&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pageNumber += 1}&with_watch_providers=${provider}&watch_region=GB&with_watch_monetization_types=flatrate`).then((response) => {
+        setMovies4(response.data.results)
+      }).catch((err) => {
+        console.log(err)
+      })
+    },
   [])
   return (
     <div className="list">
       <div className="wrapper">
        <div className="container">
         {movies.map((movie, index) => {
+          return <MovieItem key={index} {...movie}/>
+        })}
+        {movies2.map((movie, index) => {
+          return <MovieItem key={index} {...movie}/>
+        })}
+        {movies3.map((movie, index) => {
+          return <MovieItem key={index} {...movie}/>
+        })}
+        {movies4.map((movie, index) => {
           return <MovieItem key={index} {...movie}/>
         })}
         </div>
